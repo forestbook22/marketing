9d8e8266fcaa819770631dc9200a8c5edc8a50e2
forestbook22 <ryousuke991010@gmail.com>
2020-11-19 23:27:37 +0900

    Modify css

--- src/controllers/users/UsersUpdateServlet.java
+++ src/controllers/users/UsersUpdateServlet.java
@@ -64,7 +64,7 @@ public class UsersUpdateServlet extends HttpServlet {
                     );
         }
 
-        u.setAdmin_flag(Integer.parseInt(request.getParameter("admmin_flag")));
+        u.setAdmin_flag(Integer.parseInt(request.getParameter("admin_flag")));
         u.setUpdated_at(new Timestamp(System.currentTimeMillis()));
         u.setDelete_flag(0);
 
